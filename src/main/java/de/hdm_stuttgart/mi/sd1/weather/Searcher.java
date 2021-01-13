6f5c6571914bdb10cf09e15061a0c0b180512c60
Jakob Günster <jg160@hdm-stuttgart.de>
2021-01-13 19:08:09 +0100

    Searcher methode searchCity

--- src/main/java/de/hdm_stuttgart/mi/sd1/weather/Searcher.java
+++ src/main/java/de/hdm_stuttgart/mi/sd1/weather/Searcher.java
@@ -3,21 +3,23 @@ package de.hdm_stuttgart.mi.sd1.weather;
 import de.hdm_stuttgart.mi.sd1.weather.cities.Cities;
 import de.hdm_stuttgart.mi.sd1.weather.cities.City;
 
+import java.util.Arrays;
+
 public class Searcher {
 
     public static City[] searchCity(String searchString) throws Exception {
-        if (searchString.toCharArray().length <= 2) {
-
-        }
-
 
         City[] cities = new Cities().cities;
+        City[] foundCities = new City[0];
+        int i=0;
 
         for (City city : cities) {
-            if (city.getName().toLowerCase().equals(searchString.toLowerCase())) {
-                return cities;
+            if (city.getName().toLowerCase().contains(searchString.toLowerCase())) {
+                foundCities=Arrays.copyOf(foundCities, foundCities.length+1);
+                foundCities[i]=city;
+                i++;
             }
         }
-        return null;
+        return foundCities;
     }
 }
